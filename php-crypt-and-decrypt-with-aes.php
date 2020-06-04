8315f35d22cb71679b16ef103af16c418e3d9aef
MathiAs2Pique <45072138+MathiAs2Pique@users.noreply.github.com>
2020-06-04 14:10:08 +0400

    eupdèt

--- php-crypt-and-decrypt-with-aes.php
+++ php-crypt-and-decrypt-with-aes.php
@@ -7,7 +7,7 @@ function crypter($tocrypt, $clechiffrement) {
     $ciphertext_raw = openssl_encrypt($plaintext, $cipher, $clechiffrement, $options=OPENSSL_RAW_DATA, $iv);
     $hmac = hash_hmac('sha256', $ciphertext_raw, $clechiffrement, $as_binary=true);
     $crypted = base64_encode( $iv.$hmac.$ciphertext_raw ); //Texte chiffré
-    return $crypted;
+    return $crypted; //on return le texte chiffre
 };
 
 function decrypter($hash, $clechiffrement) {
@@ -16,9 +16,8 @@ function decrypter($hash, $clechiffrement) {
     $iv = substr($c, 0, $ivlen);
     $hmac = substr($c, $ivlen, $sha2len=32);
     $ciphertext_raw = substr($c, $ivlen+$sha2len);
-    $decrypted = openssl_decrypt($ciphertext_raw, $cipher, $clechiffrement, $options=OPENSSL_RAW_DATA, $iv);
-    $calcmac = hash_hmac('sha256', $ciphertext_raw, $clechiffrement, $as_binary=true); //Texte déchiffré
-    return $decrypted;
+    $decrypted = openssl_decrypt($ciphertext_raw, $cipher, $clechiffrement, $options=OPENSSL_RAW_DATA, $iv); //texte déchiffré
+    return $decrypted; //on return le texte déchiffré
 };
 
 ?>
